ab698591cdd3e4aaf513ab78abce6bc03734c5d9
stolbov <stolbov@informatica.ru>
2015-08-21 14:33:46 +0300

    Настроен сервер с релоадом

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,4 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var url = require("file!./file.png");
-
 module.exports = {
-
-    // entry: "./src/index.js",
 
     entry: {
         app: ["webpack/hot/dev-server", "./src/js/app.js"]
@@ -14,29 +9,8 @@ module.exports = {
         filename: "bundle.js"
     },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'My App',
-            filename: 'index.html'
-        })
-    ],
-
     module: {
         loaders: [
-            // {
-            //     test: /\.(png|jpg)$/,
-            //     // loader: 'url-loader?limit=10000&name=/src/img/[name].[ext]'
-            //     loader: "url-loader"
-            // },
-            // {
-            //     test: /\.(png|jpg|html)$/,
-            //     loader: 'file-loader?name=img/[name].[ext]'
-            // },
-            {
-                test: /\.jpe?g$|\.gif$|\.png$/i,
-                loader: "file-loader"
-            }
-
         ]
     },
 
