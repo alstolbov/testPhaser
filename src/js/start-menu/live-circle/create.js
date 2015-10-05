877a9253b5ee07de6af77f677bd7ee5a820bdb56
stolbov <stolbov@informatica.ru>
2015-10-05 11:26:17 +0300

    vawe tweens

--- src/js/start-menu/live-circle/create.js
+++ src/js/start-menu/live-circle/create.js
@@ -25,5 +25,31 @@ module.exports = function () {
     // this.game.input.onDown.add(popup.openWindow, popup);
 
     this.game.state.start('puzzle-game-level');
-
+    // var square = this.game.add.graphics();
+    // square.beginFill(0x000000);
+    // square.drawRect(0, 40, 400, 40);
+    // // square.drawCircle(0, 50, 20);
+    // square.position.set(0, 100);
+    // var tween = this.game.add.tween(square).to({
+    //     x: [100, 200, 100, 0],
+    //     y: [60, 100, 140, 100]
+    // }, 3000);
+    // tween.interpolation(function(v, k){
+    //     return Phaser.Math.bezierInterpolation(v, k);
+    // });
+    // tween.repeat(Infinity);
+    // tween.start();
+    // var square2 = this.game.add.graphics();
+    // square2.beginFill(0x20732F);
+    // square2.drawRect(0, 50, 400, 50);
+    // square2.position.set(0, 100);
+    // var tween2 = this.game.add.tween(square2).to({
+    //     x: [100, 200, 100, 0],
+    //     y: [50, 100, 150, 100]
+    // }, 3000).delay(500);
+    // tween2.interpolation(function(v, k){
+    //     return Phaser.Math.bezierInterpolation(v, k);
+    // });
+    // tween2.repeat(Infinity);
+    // tween2.start();
 };
