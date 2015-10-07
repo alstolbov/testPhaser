84aa6273591bd9e43a3df327da7f169dc4ccdb92
stolbov <stolbov@informatica.ru>
2015-10-07 11:06:31 +0300

    prepare animation for ship level

--- src/js/start-menu/live-circle/create.js
+++ src/js/start-menu/live-circle/create.js
@@ -25,6 +25,8 @@ module.exports = function () {
     // this.game.input.onDown.add(popup.openWindow, popup);
 
     this.game.state.start('puzzle-game-level');
+
+    // vawe animation
     // var square = this.game.add.graphics();
     // square.beginFill(0x000000);
     // square.drawRect(0, 40, 400, 40);
@@ -52,4 +54,65 @@ module.exports = function () {
     // });
     // tween2.repeat(Infinity);
     // tween2.start();
+
+    // // cloud animation
+    // var square = this.add.sprite(200, 100, 'markerSprite');
+    // square.anchor.setTo(0.5, 0.5);
+    // var tween = this.game.add.tween(square)
+    //     .to({x: -20}, 3000, null, true, 0)
+    // ;
+    // tween.onComplete.add(
+    //     function () {
+    //         square.x = 200;
+    //         tween.start();
+    //     },
+    //     this
+    // );
+
+
+    // //ship animation
+    // var square = this.add.sprite(100, 100, 'markerSprite');
+    // square.anchor.setTo(0.5, 0.5);
+    // function tweener (angle, y, callback) {
+    //     var tween = _this.game.add.tween(square)
+    //         .to({
+    //             angle: angle,
+    //             y: y
+    //             }, 1000, null, true, 0//, Infinity
+    //         )
+    //     ;
+    //     tween.onComplete.add(callback, _this);
+    // }
+    // function tw1 () {
+    //     square.angle = 20;
+    //     tweener('-20', 110, function () {
+    //         square.angle = 0;
+    //         tw2 ();
+    //     });
+    // }
+    // function tw2 () {
+    //     tweener('-20', 100, function () {
+    //         square.angle = -20;
+    //         tw3 ();
+    //     });
+    // }
+    // function tw3 () {
+    //     tweener('+20', 90, function () {
+    //         square.angle = 0;
+    //         tw4 ();
+    //     });
+    // }
+    // function tw4 () {
+    //     tweener('+20', 100, function () {
+    //         square.angle = 20;
+    //         tw1 ();
+    //     });
+    // }
+
+    // setTimeout(
+    //     function () {
+    //         tw1();
+    //     }, 1000
+    // );
+
 };
